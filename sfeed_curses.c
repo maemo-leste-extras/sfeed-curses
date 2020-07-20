maemo-leste-extras/sfeed-curses

commit d4a647803cc05e8f81c14e15347c908c8c856b5c

feeds_load: do not close and reopen the current feed needlessly

sfeed_curses.c
@@ -1159,14 +1159,14 @@ feeds_load(struct feed *feeds, size_t nfeeds)
 		}
 
 		/* load first items, because of first selection or stdin. */
-		if (i == 0 || f == curfeed)
+		if (i == 0 || f == curfeed) {
 			feed_load(f, f->fp);
-		else
+		} else {
 			feed_count(f, f->fp);
-
-		if (f->path && f->fp) {
-			fclose(f->fp);
-			f->fp = NULL;
+			if (f->path && f->fp) {
+				fclose(f->fp);
+				f->fp = NULL;
+			}
 		}
 	}
 }

commit b805c629c70701bb49dbd89d6a0cb6277a8ed7fa

on reload all always close and reopen the current feed (if its a path)
Fixes/improves upon a regression from an optimization in
d4a647803cc05e8f81c14e15347c908c8c856b5c

sfeed_curses.c
@@ -1248,6 +1248,7 @@ feeds_reloadall(void)
 
 	pos = panes[PaneItems].pos; /* store numeric position */
 	readurls();
+	feeds_set(curfeed); /* close and reopen feed if possible */
 	feeds_load(feeds, nfeeds);
 	/* restore numeric position */
 	pane_setpos(&panes[PaneItems], pos);