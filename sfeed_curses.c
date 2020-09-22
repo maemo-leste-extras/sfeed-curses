bf6029c572fa7388d22622fe270ad16b6e479c45
Michael Forney <mforney@mforney.org>
2020-09-22 01:49:54 -0700

    Only highlight selected line in focused pane

--- sfeed_curses.c
+++ sfeed_curses.c
@@ -36,7 +36,7 @@
 #define THEME_ITEM_NORMAL()           do {                            } while(0)
 #define THEME_ITEM_FOCUS()            do {                            } while(0)
 #define THEME_ITEM_BOLD()             do { attrmode(ATTR_BOLD_ON);    } while(0)
-#define THEME_ITEM_SELECTED()         do { attrmode(ATTR_REVERSE_ON); } while(0)
+#define THEME_ITEM_SELECTED()         do { if (p->focused) attrmode(ATTR_REVERSE_ON); } while(0)
 #define THEME_SCROLLBAR_FOCUS()       do {                            } while(0)
 #define THEME_SCROLLBAR_NORMAL()      do { attrmode(ATTR_FAINT_ON);   } while(0)
 #define THEME_SCROLLBAR_TICK_FOCUS()  do { attrmode(ATTR_REVERSE_ON); } while(0)
