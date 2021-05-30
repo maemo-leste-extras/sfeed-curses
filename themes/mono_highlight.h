7b644e71a5a08ba26bd420605310cdda33dac95f
Hiltjo Posthuma <hiltjo@codemadness.org>
2021-05-30 12:30:18 +0200

    mono_highlight: show items as bold when selected.
    bold + faint is not supported though, so new items won't be visible there.

--- themes/mono_highlight.h
+++ themes/mono_highlight.h
@@ -3,8 +3,8 @@
    The combination bold with faint generally does not work either. */
 #define THEME_ITEM_NORMAL()           do {                            } while(0)
 #define THEME_ITEM_FOCUS()            do {                            } while(0)
-#define THEME_ITEM_BOLD()             do { if (!selected) attrmode(ATTR_BOLD_ON); } while(0)
-#define THEME_ITEM_SELECTED()         do { if (!p->focused) attrmode(ATTR_FAINT_ON); attrmode(ATTR_REVERSE_ON); } while(0)
+#define THEME_ITEM_BOLD()             do { if (p->focused || !selected) attrmode(ATTR_BOLD_ON); } while(0)
+#define THEME_ITEM_SELECTED()         do { attrmode(ATTR_REVERSE_ON); if (!p->focused) attrmode(ATTR_FAINT_ON); } while(0)
 #define THEME_SCROLLBAR_FOCUS()       do {                            } while(0)
 #define THEME_SCROLLBAR_NORMAL()      do { attrmode(ATTR_FAINT_ON);   } while(0)
 #define THEME_SCROLLBAR_TICK_FOCUS()  do { attrmode(ATTR_REVERSE_ON); } while(0)
